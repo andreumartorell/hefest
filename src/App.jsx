09f720a7f5a0a7794ffe3b92d649956b9d2f24da
Andreu Martorell <anmartu@gmail.com>
2023-10-19 12:12:09 +0200

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import {Link, Route, Routes} from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      Hola
+    </div>
+    </>
+  )
+}
+
+export default App
