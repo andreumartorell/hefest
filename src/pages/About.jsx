bde0e7f713e5450b8c025f4ed60a9947c14fdae2
Andreu Martorell <anmartu@gmail.com>
2023-11-01 22:24:41 +0100

    1/11

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,12 @@ let arr = ['ca', 'es', 'en']
 export const [ About, About_es, About_en ] = arr.map(() => {
   return ({text}) => {
     return (
+      <>
+      <section>
       <h1>{text.about.title}</h1>
+        <p>Som una empresa líder al sector de mecanitzats, i el nostre compromís amb la qualitat i l'eficiència es reflecteix a cada projecte que abordem. Amb un parc de maquinària avançat i tecnologia punta, estem equipats per abordar projectes de mecanització de qualsevol escala i complexitat. El nostre equip d'experts altament capacitats treballa amb les nostres màquines de precisió per crear els millors components.</p>
+      </section>
+      </>
     )
 }
 })
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ export const [ Home, Home_es, Home_en ] = arr.map(() => {
       <>
       <div className='container'>
         <section className='section'>
-          <h1 className='section__h1'>{text.home.title}</h1>
+          <h1 className='hero-h1'>{text.home.title}</h1>
           <div className='section__copy'>
             <div className='section__item-cont'>
             <img className='section__img' width="1000px" src="/home/seu.jpg" alt="Seu" />
@@ -23,18 +23,23 @@ export const [ Home, Home_es, Home_en ] = arr.map(() => {
       </div>
         <section className='frase'>
           <div className='frase__text'>Ho fem tot amb carinyo</div>
-          <div className='frase__deco'></div>
         </section>
       <div className='container'>
         <section className='section'>
-        <div className='section__copy section__copy--reverse'>
+          <h1 className='section__h1'>Com treballem</h1>
+          <div className='section__copy section__copy--reverse'>
             <div className='section__item-cont'>
-             <img className='section__img' width="1000px" src="/home/seu.jpg" alt="Seu" />
+             <img className='section__img' width="1000px" src="/home/mach.jpg" alt="Pieces" />
             </div>
             <div className='section__item-cont'>
-              <p className="section__text">{text.home.info_3}</p>
+              <p className="section__text section__text--nomarg">{text.home.info_3}</p>
               <Link to={`${r}/contact`} className='section__button'>Veure maquinària</Link>
             </div>
+          </div>
+          <div className='product'>
+            <img width='300px' className='product__item' src="/home/p2-2.jpg" alt="Product" />
+            <img width='300px' className='product__item' src="/home/p3.jpg" alt="Product" />
+            <img width='300px' className='product__item' src="/home/p1.jpg" alt="Product" />
           </div>
         </section>
       </div>
